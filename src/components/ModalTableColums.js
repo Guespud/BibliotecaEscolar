a737bc8c8a1ec859f20017999bbb843ba1cec510
Alejandro Guespud <alejo.habbacuc@gmail.com>
2020-11-21 14:08:02 -0500

    creacion, navbar

---
+++ src/components/ModalTableColums.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ModalTableColums = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ModalTableColums;
---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  palette: {
+    primary: {
+      main: '#008394',
+    }
+  },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            BIBLIOTECA ESCOLAR
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
